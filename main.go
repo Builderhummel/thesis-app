f774af1e08d7d8f1093b194fdf79649802d44787
Max Kretschmann <max.kretschmann@tuhh.de>
2025-01-14 15:08:01 +0100

    Clean-up: main.go

--- main.go
+++ main.go
@@ -34,48 +34,6 @@ func init_config() {
 func main() {
 	var err error
 	_ = err
-	/**
-	//DB stuff
-	var dbc db_model.DBController
-	_, err := dbc.OpenConnection()
-	if err != nil {
-		log.Fatalf("could not open connection: %v", err)
-	}
-	defer dbc.CloseConnection()
-
-	check, err := dbc.CheckIfDatabaseIsInitialized()
-	if err != nil {
-		log.Fatalf("could not check if database is initialized: %v", err)
-	}
-	if !check {
-		err = dbc.InitDatabase()
-		if err != nil {
-			log.Fatalf("could not init database: %v", err)
-		}
-	}
-	*/
-
-	/*
-		//LDAP TEST
-		var auser auth_controller.AuthUser
-		err = auser.LDAP_authenticate(os.Getenv("LUSERNAME"), os.Getenv("LPASSWORD"))
-		if err != nil {
-			if ldapErr, ok := err.(*ldap.Error); ok {
-				switch ldapErr.ResultCode {
-				case ldap.LDAPResultInvalidCredentials:
-					println("Hi")
-					log.Fatalf("invalid credentials: %v", ldapErr)
-				}
-			}
-			log.Fatalf("could not authenticate: %v", err)
-		}
-		log.Printf("UID: %v", auser.UID)
-		log.Printf("Name: %v", auser.Name)
-		log.Printf("Email: %v", auser.Email)
-
-		os.Exit(0)
-		//LDAP TEST
-	*/
 
 	//Gin stuff
 	r := gin.New()
